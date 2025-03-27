e434ce78393f4daff2ad9cae8db7d154025633b9
Melanya25 <o.melanya@mail.ru>
2025-03-27 18:37:19 +0300

    Update output.c

--- output.c
+++ output.c
@@ -1,17 +1,19 @@
 #include "output.h"
 
-void output_matrix_to_file(const Matrix* matrix, const char* filename) {
+//функция вывода матрицы из файла
+void output_matrix_to_file(const Matrix* matrix, const char* file_name) {
     if (!matrix) return;
 
-    FILE* file = fopen(filename, "w");
+    FILE* file = fopen(file_name, "w"); //открытие файла для письма
     if (!file) return;
 
+    //перебор элементов строк и столбцов матрицы
     for (int i = 0; i < matrix->rows; i++) {
         for (int j = 0; j < matrix->cols; j++) {
             fprintf(file, "%lf\t", matrix->data[i][j]);
         }
-        fprintf(file, "\n");
+        fprintf(file, "\n"); //вывод файла
     }
 
-    fclose(file);
+    fclose(file); //закрытие файла
 }
